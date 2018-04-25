de7589bf5dcff548b835bc801dba1c73c43a359a
Al Ashby <alam9c@mail.umkc.edu>
2018-04-25 15:50:35 -0500

    Additional Inserts and Alter
    Please execute the alter (update.sql) query first. More movies, added customers with addresses and phone numbers.

---
+++ insertaddresses.sql
@@ -0,0 +1,9 @@
+INSERT INTO video_rental.Address
+VALUES
+('00001','665','Exarch Drive','','Kansas City','MO','64110','USA'),
+('00002','718','North Cottage Boulevard','','Independence','MO','64050','USA'),
+('00003','482','Jefferson Avenue','3C','Litchfield','IL','62056','USA'),
+('00004','1967','Spencer Mansion Lane','','Raccoon City','MO','64517','USA'),
+('00005','1967','Spencer Manson Lane','','Raccoon City','MO','64517','USA'),
+('00006','1967','Spencer Mansion Lane','','Raccoon City','MO','64517','USA'),
+('00007','848','Baker Ranch Lane','','Dulvey','LA','70501','USA');
---
+++ insertcustomers.sql
@@ -0,0 +1,9 @@
+INSERT INTO video_rental.Customer 
+VALUES 
+('00001',23,'Owain','Dark',NULL,NULL),
+('00002',46,'William','Blazkowics','J',NULL),
+('00003',20,'Emeline','Browne',NULL,NULL),
+('00004',58,'Barry','Burton',NULL,NULL),
+('00005',44,'Jill','Valentine',NULL,NULL),
+('00006',45,'Chris','Redfield',NULL,NULL),
+('00007',56,'Jack','Baker',NULL,NULL);
